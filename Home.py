1fb0bec964c2e8d55ee8f2cfed578cc7a825bc81
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-25 17:53:11 +0330

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,7 +12,7 @@ st.title('Activity of NEAR Developers')
 # Content
 c1, c2 = st.columns(2)
 
-#c1.image(Image.open('Images/near2-logo.png'))
+#c1.image(Image.open('Images/COVER.JPG'))
 
 st.subheader('📃 Introduction')
 
