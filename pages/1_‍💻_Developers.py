bf2efb9414ee242cda7b9e95ab6e2f4031eb44e9
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-26 05:01:32 +0330

    Update 1_‍💻_Developers.py

--- pages/1_‍💻_Developers.py
+++ pages/1_‍💻_Developers.py
@@ -189,6 +189,12 @@ fig = px.bar(df, x='Developer', y='Days Active', color='Developer', title='Top 2
 fig.update_layout(showlegend=False, xaxis_title=None, legend_title='', yaxis_title='Days Count', xaxis={'categoryorder':'total ascending'})
 st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
 
+df = Heat_map
+fig = px.density_heatmap(df, x='Hour', y='Day Name', z='Developer Count', histfunc='avg', title='Developers Count Heat map, Days of Week vs. Hours of Day', nbinsx=24)
+fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title=None, xaxis={'dtick': 1}, yaxis={'dtick': 1}, coloraxis_colorbar=dict(title='DEVs Count'))
+fig.update_yaxes(categoryorder='array', categoryarray=Heat_map)
+st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+
 # Contracts Analysis
 subtab_Daily, subtab_Weekly, subtab_Monthly, subtab_Yearly = st.tabs(['Daily', 'Weekly', 'Monthly','Yearly'])
 
