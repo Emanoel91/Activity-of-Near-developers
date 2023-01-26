7454dfd45538bc74945902b2681d5bc1b67676b2
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-26 07:14:48 +0330

    Update Home.py

--- Home.py
+++ Home.py
@@ -21,10 +21,12 @@ st.write(
     """
 Today, if someone wants to use a simple application or game launched on the blockchain platform, he has to go through many steps; Therefore, the Near network has put its main focus on the usability and user-friendliness of its platform; Developers and programmers and end users of products launched on the Near platform should feel better and more comfortable when using it. This network is very similar to Ethereum in terms of idea and is a platform for launching decentralized applications. The Near protocol is similar to the Ethereum network; With the difference that this chain tries to provide higher usability, scalability, and ease, and finally be a completely decentralized network. The NEAR protocol provides a platform where programmers can freely present their programs. With such programs, we will have a better world where people are fully in control of their assets, be it money, personal information, etc.
 
-🤔 What is the purpose of creating the Near protocol?
+🤔 **What is the purpose of creating the Near protocol?**
+
 The main goal of the NEAR network is to create an infrastructure for creating a new Internet. In the new internet, it will be harder for big companies to access people's personal information. Countries cannot ban some programs and destroy people's business in this system. A world of freedom where everyone can act freely. Of course, this goal of the Near network is not a new concept; In fact, Satoshi Nakamoto also pursued the same goal by introducing Bitcoin in 2008. But Bitcoin has created this freedom only in the financial sphere. But like many other networks, Near seeks to expand this freedom to other aspects of human life.
 
-👌 Near protocol features
+👌 **Near protocol features**
+
 1️⃣ The speed of transactions in Near network is high and the transaction fees are low. The reason for this is that Near protocol uses sharding technology.
 
 2️⃣ Scalability is an important concept in the blockchain world. Near protocol uses sharding technology to solve the scalability problem. In this model, a transaction does not need to be checked by all network nodes. With sharding, a network is transformed into several parallel networks that check transactions simultaneously, so t hat each transaction is checked in a shard. As a result, only nodes present in a shard will check and confirm this transaction, and other nodes present in other shards of the network will not check this transaction. This idea makes the capacity of the network increase significantly.
