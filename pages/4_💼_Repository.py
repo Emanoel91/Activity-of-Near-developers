2d43e0a1e08d57eaf19c4f14b0003b6d8ace0da3
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-26 07:06:59 +0330

    Update 4_💼_Repository.py

--- pages/4_💼_Repository.py
+++ pages/4_💼_Repository.py
@@ -140,10 +140,10 @@ Top_20_Developers_Based_on_Number_of_Likes = get_data('Top 20 Developers Based o
 Top_20_Developers_Based_on_Number_of_Days_of_Activity = get_data('Top 20 Developers Based on Number of Days of Activity')
 Heat_map = get_data('Heat map')
 
-df = Top_20_Repositories_With_Most_Pull_Requests_Count 
-fig = px.bar(df, x='REPO', y='Pull Requests Count', color='REPO', title='Top 20 Repositories With Most Pull Requests Count', log_y=False)
-fig.update_layout(showlegend=False, xaxis_title=None, legend_title='', yaxis_title='PRs Count', xaxis={'categoryorder':'total ascending'})
-st.plotly_chart(fig, use_container_width=True, theme=theme_plotly) 
+#df = Top_20_Repositories_With_Most_Pull_Requests_Count 
+#fig = px.bar(df, x='REPO', y='Pull Requests Count', color='REPO', title='Top 20 Repositories With Most Pull Requests Count', log_y=False)
+#fig.update_layout(showlegend=False, xaxis_title=None, legend_title='', yaxis_title='PRs Count', xaxis={'categoryorder':'total ascending'})
+#st.plotly_chart(fig, use_container_width=True, theme=theme_plotly) 
 
 df = Heat_map
 fig = px.density_heatmap(df, x='Hour', y='Day Name', z='Repository Count', histfunc='avg', title='Repository Count Heat map, Days of Week vs. Hours of Day', nbinsx=24)
